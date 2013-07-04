d4bf67f03ac28413bc24074be990fe2b5637695a
morrillo <gustavo.orrillo@moldeo.coop>
2013-07-04 19:42:31 -0300

    web.js updated

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var buffer = new Buffer(256);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  console.log('Entro 1');
+  var data = fs.readFileSync('index.html', 'utf8') 
+  var buffer = new Buffer(data,'utf8');
+  response.send(buffer.toString('utf8'));
+  console.log(data);
 });
 
 var port = process.env.PORT || 5000;
